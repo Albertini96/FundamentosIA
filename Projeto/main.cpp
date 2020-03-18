6221d9db68c90e9b1bbbaae64bfd425284e3e0cc
Albertini96 <guilhermeoliveira@arttu.co>
2020-03-17 21:26:29 -0300

    Initial Commit

---
+++ Projeto/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Projeto
+//
+//  Created by Guilherme Albertini on 17/03/20.
+//  Copyright © 2020 Guilherme Albertini. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
